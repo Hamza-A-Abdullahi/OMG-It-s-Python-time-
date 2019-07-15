ea7b09fb45787c7e50f5387de05ea28be1980a7a
Hamza Abdullahi <HAbdullahi@spartaglobal.com>
2019-07-15 13:56:09 +0100

    taking input from user

---
+++ user_details_funny_exercise.py
@@ -0,0 +1,17 @@
+# In this file is where I Will do my exercise
+#SUDO
+#get user name
+#save to a variable
+#get user date
+#get funny fact from user
+#then print it out
+
+user_name= input("what is your full name:  ")
+date= input("Enter the most important date to you: ")
+date_reason= input("why is  this date important to you? ")
+favorite_pokemon = input("What is your favorite pokemon? ")
+funny_fact= input("Tell me a funny fact about yourself: ")
+
+
+print("hi, my name is ",user_name, "this date is important, ",
+      date,"the reason why is because, ",date_reason,"favorite pokemon is, ",favorite_pokemon,funny_fact)
