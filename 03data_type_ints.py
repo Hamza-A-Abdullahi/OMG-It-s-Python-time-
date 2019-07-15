5d3c3c1f45c443e6024f0928016b53de11ef2b21
Hamza Abdullahi <HAbdullahi@spartaglobal.com>
2019-07-15 12:12:12 +0100

    lesson on casting

---
+++ 03data_type_ints.py
@@ -0,0 +1,52 @@
+#numericals- integer, big integer and complex numbers
+
+#whole numbers
+
+# print (13)
+# print (type(13)) # not the same as print (type('13'))  ---> STRING
+
+#Float are composite numbers
+
+# print (13.29984)
+# print (type(13.2884))
+
+#operators
+
+# num_a = 1324
+# num_b =442
+# print( num_a- num_b)
+# print( num_a + num_b)
+# print( num_a / num_b)
+# print( num_a * num_b)
+# print( num_a % num_b)
+
+#logical operators
+#check if equal==
+
+# a=10
+# b=12
+#
+# print(a==b)
+#
+# # checking if bigger and smaller than
+#
+# print (a> b)
+# print(a>9)
+# print ('.....')
+# print (b<a)
+# print(b<15)
+#
+# print ('.....')
+# #check if not something
+#
+# print (a !=9)
+
+#increment
+
+counter= 10
+print(counter)
+counter += 1
+print (counter)
+counter += 12
+print (counter)
+
---
+++ 04_casting.py
@@ -0,0 +1,18 @@
+#casting is when you change the data type of an object
+#e.g. we can cast strings into integers or intergers into strings
+
+# number_string = '10'
+# print (type(number_string))
+# #now changing to interger using int ()
+# number_int= int (number_string)
+# print(type(number_int))
+
+#Casting integer class to string
+
+print ("now let us do the same bt for string into integers")
+number_int2= 11
+print(type (number_int2))
+#change to string to using str()
+
+number_int2_str = str(number_int2)
+print(type(number_int2_str))
