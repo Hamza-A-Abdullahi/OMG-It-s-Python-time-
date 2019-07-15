7cab4da2152363d8313225b62ff28fb6b5c9a31b
Hamza Abdullahi <HAbdullahi@spartaglobal.com>
2019-07-15 11:26:15 +0100

    change python file name. New data type ints

---
+++ 02_datatype.py
@@ -0,0 +1,19 @@
+#specify variable data type
+# we can use type() to check the data type
+
+# string_a = 'hello there'
+# name_person = 'Juan Pier'
+#
+# print (string_a + ' '+ name_person)
+#
+# ##strip- remove trailing white spaces
+#
+# string_num = '88896 '
+# print(string_num)
+# print(string_num.strip())   #remove white space at the end
+
+# get user input
+# user_first= input('what is your first name:  ')
+# user_surename= input('what is your family name:  ')
+# print (f"hello {user_first} {user_surename}  Welcome to my world")
+
