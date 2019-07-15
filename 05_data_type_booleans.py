dbff9eb6e473682bbd58a42fbd7b8e285fc8fbce
Hamza Abdullahi <HAbdullahi@spartaglobal.com>
2019-07-15 14:34:55 +0100

    boolean testing

---
+++ 05_data_type_booleans.py
@@ -0,0 +1,27 @@
+# #Logical  **AND** & **OR**
+#
+# test = []
+# print(test,'is',bool(test))
+#
+# test = [0]
+# print(test,'is',bool(test))
+#
+# test = 0.0
+# print(test,'is',bool(test))
+#
+# test = None
+# print(test,'is',bool(test))
+#
+# test = True
+# print(test,'is',bool(test))
+#
+# test = 'Easy string'
+# print(test,'is',bool(test))
+#
+
+#bolean and numbers
+
+print ("print bool values of numbers")
+print (bool(0))
+print (bool(13))
+print (bool(1))
