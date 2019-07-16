1124df183f447c036b9c180a7f2aeefb246a87ac
Hamza Abdullahi <HAbdullahi@spartaglobal.com>
2019-07-16 09:49:55 +0100

    manage to get the story output in newline

--- homework_exercise.py
+++ homework_exercise.py
@@ -7,18 +7,25 @@
 my_dict = dict()
 
 key = input("Enter the title of  beginning chapter of your story: \n")
-value = input("type the beginning of story: \n")
+value = input("type the beginning of story: ")
 
 key_m = input("Enter the  title of  middle chapter of your story: \n")
 value_m = input("type the middle of story: \n ")
 
 key_end = input("Enter the title of end chapter of your story: \n")
-value_end = input("type the end of story: \n ")
-
-my_dict[key] = value # chapter 1 beginning
-
-my_dict[key_m] = value_m  #chapter 2 middle
-
-my_dict[key_end] = value_end  #chapter 3 the end
-
+value_end = input("type the end of story: ")
+print('ended input, starting to build the dictionary')
 print(my_dict)
+my_dict[key] = value # chapter 1 beginning
+print(my_dict)
+my_dict[key_m] = value_m  #chapter 2 middle
+print(my_dict)
+my_dict[key_end] = value_end  #chapter 3 the end
+print(my_dict)
+
+print("The Beginning, " + my_dict[key])
+print("The Middle, " + my_dict[key_m])
+print("the End, " + my_dict[key_end])
+
+
+print ("--------------The END-----------")
